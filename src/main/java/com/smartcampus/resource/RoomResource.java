fb7101ab0cc01eb6498b7a506f5bda2978791bd2
hasarailleperuma <hasarailleperuma@gmail.com>
2026-04-20 15:17:14 +0530

    Add getRoomById endpoint to RoomResource to retrieve rooms by ID

--- src/main/java/com/smartcampus/resource/RoomResource.java
+++ src/main/java/com/smartcampus/resource/RoomResource.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -31,5 +32,16 @@ public class RoomResource {
     public Response createRoom(Room room) {
         dataStore.getRooms().put(room.getId(), room);
         return Response.status(Response.Status.CREATED).entity(room).build();
+    }
+
+    @GET
+    @Path("/{roomId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRoomById(@PathParam("roomId") String roomId) {
+        Room room = dataStore.getRooms().get(roomId);
+        if (room == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(room).build();
     }
 }
